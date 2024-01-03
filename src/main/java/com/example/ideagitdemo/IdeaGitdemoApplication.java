85323e6f908c8df36883801cafc8cb759c5ec5cc
Dong <dxyvictory688@163.com>
2024-01-03 10:50:13 +0800

    first commit

---
+++ src/main/java/com/example/ideagitdemo/IdeaGitdemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.ideagitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGitdemoApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(IdeaGitdemoApplication.class, args);
+        System.out.println("web程序测试成功");
+        System.out.println("git 配置成功");
+    }
+
+}
---
+++ src/test/java/com/example/ideagitdemo/IdeaGitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideagitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaGitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
